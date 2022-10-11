3365422b6c6383bb62d827e49b8240c35e3e0517
Duncan <duncan.cornish.1999@gmail.com>
2022-10-11 16:35:32 +0100

    make tests work

---
+++ src/components/LoginForm/LoginForm.test.js
@@ -0,0 +1,19 @@
+
+import { screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { default as LoginForm } from './index'
+
+describe('LoginForm', () => {
+    beforeEach(() => {
+        render(
+            <Router>
+                <LoginForm />
+            </Router>
+        )
+    })
+
+    test('renders the form', () => {
+        let loginForm = screen.getByRole("form")
+        expect(loginForm).toBeTruthy
+    })
+})
--- src/components/Logo/Logo.test.js
+++ src/components/Logo/Logo.test.js
@@ -1,9 +1,14 @@
 import { screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { default as Logo } from './index'
 
 describe('Logo', () => {
     beforeEach(() => {
-        render(<Logo/>)
+        render(
+            <Router>
+                <Logo />
+            </Router>
+        )
     })
 
     test('renders a heading with trivia rangers in it', () => {
