4ab821bb0793704eaaf8f0a4dcde9637f66a4a3a
Stefan Mircea <stefancmircea@gmail.com>
2022-10-12 12:13:45 +0100

    start implemet questions

--- src/pages/Questions/index.js
+++ src/pages/Questions/index.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 
 import './style.css'
-import { Timer } from '../../components'
 
 const Questions = () => {
+    const [counter, setCounter] = useState(30);
+    // const [shuffledAnswers, setShuffledAnswers] = useState([]);
+    const [selectedOption, setSelectedOption] = useState('');
+    const [isGameOver, setIsGameOver] = useState(false);
+    const dispatch = useDispatch();
+    const gameState = useSelector(state => state.gameState);
+    const socket = useSelector(state => state.socket);
+    const clientUser = localStorage.getItem('username');
+    // const proRef = useRef();
+
+    useEffect(() => {
+        counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    }, [counter]);
+
     return (
         <div>
 
-            <div className='timer'><Timer/></div>
-  
+            <div className='timer'>
+                <div>Time left: {counter}</div>
+            </div>
+
             <div className='question-container'>
                 <h1 className='question'>QUESTION GOES HERE</h1>
             </div>
