1a7995aee4a73e2f60c1e6a9652636a8a72d0870
Duncan <duncan.cornish.1999@gmail.com>
2022-10-12 16:22:01 +0100

    fix component roles

--- src/components/InGameLeaderboard/index.js
+++ src/components/InGameLeaderboard/index.js
@@ -3,7 +3,7 @@ import './style.css'
 
 const InGameLeaderboard = ({currentOrFinal}) => {
     return (
-        <div className='igl-container'>
+        <div role={"contentinfo"} className='igl-container'>
             <h1 style={{fontWeight: 'bold', textDecoration: 'underline', color: '#5e057e'}}>{currentOrFinal} Scores</h1>
           <ul className='leaderboard-list' role={"list"}>
             <li>beastman69 - 300</li>
